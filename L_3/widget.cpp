7a14505739c6c5545f6e066acb2db872d07a0bac
arrow <dimonsuper010@gmail.com>
2017-03-26 20:19:44 +0300

    Третя лаба

--- L_3/widget.cpp
+++ L_3/widget.cpp
@@ -6,10 +6,100 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-
+    sum = 0;
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+
+
+    QString a = QString::number(sum, 'n', 0);
+
+    QMessageBox msgBox;
+
+      msgBox.setText(a);
+      msgBox.setWindowTitle("Сума вашого замовлення");
+      msgBox.exec();
+
+
+
+}
+
+
+
+void Widget::on_checkBox_2_clicked(bool checked)
+{
+    if(checked)
+    {
+    sum+=349;}
+    else sum-=349;
+    QString a = QString::number(sum, 'n', 0);
+    ui->label_3->setText(a);
+
+}
+
+void Widget::on_checkBox_clicked(bool checked)
+{
+    if(checked)
+    {
+    sum+=369;}
+    else sum-=369;
+    QString a = QString::number(sum, 'n', 0);
+    ui->label_3->setText(a);
+}
+
+void Widget::on_checkBox_3_clicked(bool checked)
+{
+    if(checked)
+        {
+        sum+=189;}
+        else sum-=189;
+        QString a = QString::number(sum, 'n', 0);
+        ui->label_3->setText(a);
+}
+
+void Widget::on_checkBox_4_clicked(bool checked)
+{
+    if(checked)
+            {
+            sum+=179;}
+            else sum-=179;
+            QString a = QString::number(sum, 'n', 0);
+            ui->label_3->setText(a);
+}
+
+
+void Widget::on_checkBox_5_clicked(bool checked)
+{
+    if(checked)
+            {
+            sum+=299;}
+            else sum-=299;
+            QString a = QString::number(sum, 'n', 0);
+            ui->label_3->setText(a);
+}
+
+void Widget::on_checkBox_6_clicked(bool checked)
+{
+    if(checked)
+            {
+            sum+=149;}
+            else sum-=149;
+            QString a = QString::number(sum, 'n', 0);
+            ui->label_3->setText(a);
+}
+
+void Widget::on_checkBox_7_clicked(bool checked)
+{
+    if(checked)
+            {
+            sum+=199;}
+            else sum-=199;
+            QString a = QString::number(sum, 'n', 0);
+            ui->label_3->setText(a);
 }
--- L_3/widget.h
+++ L_3/widget.h
@@ -2,7 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QMessageBox>
 namespace Ui {
 class Widget;
 }
@@ -15,7 +15,26 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+private slots:
+    void on_pushButton_clicked();
+    void on_checkBox_2_clicked(bool checked);
+
+    void on_checkBox_clicked(bool checked);
+
+    void on_checkBox_3_clicked(bool checked);
+
+    void on_checkBox_4_clicked(bool checked);
+
+    \
+
+    void on_checkBox_5_clicked(bool checked);
+
+    void on_checkBox_6_clicked(bool checked);
+
+    void on_checkBox_7_clicked(bool checked);
+
 private:
+   int sum;
     Ui::Widget *ui;
 };
 
